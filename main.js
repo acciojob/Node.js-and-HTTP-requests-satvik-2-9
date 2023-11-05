57d76ae1d375251cac430f79766bc77b34c6b37c
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 15:17:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 const https = require('https');
 
-// TODO: Implement this function
+const getTodoTitle = () => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'jsonplaceholder.typicode.com',
+      path: '/todos/1',
+      method: 'GET'
+    };
+
+    const request = https.request(options, (response) => {
+      let data = '';
+
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        const actualTitle = JSON.parse(data).title;
+        resolve(actualTitle);
+      });
+    });
+
+    request.on('error', (err) => {
+      reject(`Error making HTTP request: ${err}`);
+    });
+
+    request.end();
+  });
+};
+
+if (require.main === module) {
+  getTodoTitle()
+    .then(title => console.log(title))
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
+} else {
+  module.exports = getTodoTitle;
+}
